01a24028d767a8b6befe4793214ca23bfe65bc11
DavidMercado <dmjuanes_@hotmail.es>
2022-11-02 23:48:24 -0400

    MODULOS ADICIONALES DE NODE HTTPS OS PROCESS

---
+++ modulos/http.js
@@ -0,0 +1,30 @@
+const { write } = require('fs');
+const http = require('http')
+
+http.createServer(router).listen(3000)
+
+function router(req, res) {
+    console.log('Nueva peticion!');
+    console.log(req.url);
+
+    switch (req.url) {
+        case '/hola':
+            res.write('Hola, que tal')
+            res.end();
+            break;
+    
+        default:
+            res.write('Error 404: No se lo que queires')
+            res.end();
+    }
+
+    //Cabecera
+    // res.writeHead(201, { 'Content-Type': 'text/plain' })
+
+    //Escribir respuesta al usuario
+    // res.write('Hola, ya se usar http de NodeJS')
+
+    // res.end()
+}
+
+console.log("Escuchando http en el puerto 3000");
